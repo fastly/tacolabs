1abdf8a12c1948701bd8e0618c5d37855acac01f
Matt Cone <mattcone@users.noreply.github.com>
2025-02-20 14:44:02 -0700

    Update header code in index.js

--- src/index.js
+++ src/index.js
@@ -51,9 +51,15 @@ const handler = async (event) => {
 
  // If status is not 404, send the backend response to the client
  if (backendResponse.status != 404) {
-   // Add headers to the response back to the client
-   backendResponse.headers.append("x-tacos", "🌮🌮🌮")
-   return backendResponse;
+   // Create a new headers object to avoid modifying the original
+   const newHeaders = new Headers(backendResponse.headers);
+   newHeaders.set("x-tacos", "We love tacos!");
+    
+   // Create new response with the modified headers
+   return new Response(backendResponse.body, {
+     status: backendResponse.status,
+     headers: newHeaders
+   });
  }
 }
 
